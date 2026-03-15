dfe05515234758ad1c318a6a19e23704ba45baf2
CAMISAMA0124 <qq20240214tmdg@gmail.com>
2026-03-15 14:23:30 +0800

    Fix PWA icon issues: force update SW and add more icon sizes

--- sw.js
+++ sw.js
@@ -1,26 +1,33 @@
-const CACHE_NAME = 'yc-ark-v1';
+const CACHE_NAME = 'yc-ark-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/index.css',
-  '/manifest.json',
-  '/icon.png',
-  '/js/app.js',
-  '/js/state.js'
+  './',
+  './index.html',
+  './index.css',
+  './manifest.json',
+  './icon.png',
+  './js/app.js',
+  './js/state.js'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
